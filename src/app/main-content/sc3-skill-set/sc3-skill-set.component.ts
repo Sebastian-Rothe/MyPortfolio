3f2cbee0abe9fa3b05ca96de4395ae0e2538410b
Sebastian-Rothe <sebi1995@gmx.at>
2024-09-26 19:06:14 +0200

    icons added

--- src/app/main-content/sc3-skill-set/sc3-skill-set.component.ts
+++ src/app/main-content/sc3-skill-set/sc3-skill-set.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 interface SkillIcon {
-  icons: {
     path: string;
     name: string;
     alt?: string;
-  }[];
 }
 
 @Component({
@@ -16,17 +14,18 @@ interface SkillIcon {
   styleUrl: './sc3-skill-set.component.scss',
 })
 export class Sc3SkillSetComponent {
-  skillSetIcons: SkillIcon = {
-    icons: [
-      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
+  icons: SkillIcon[] = [
+      { path:'assets/img/skill-set/Angular.svg', name: 'Angular', alt: 'Angular' },
+      { path:'assets/img/skill-set/TypeScript.svg', name: 'TypeScript', alt: 'TypeScript' },
       { path:'assets/img/skill-set/Firebase.svg', name: 'Firebase', alt: 'Firebase' },
+      { path:'assets/img/skill-set/git.svg', name: 'Git', alt: 'Git' },
+      { path:'assets/img/skill-set/JavaScript.svg', name: 'JavaScript', alt: 'JavaScript' },
       { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
-      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
-      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
-      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
-      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
-      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
-    
-    ],
-  };
+      { path:'assets/img/skill-set/Frame.svg', name: 'Material Design', alt: 'Material Design' },
+      { path:'assets/img/skill-set/html.svg', name: 'HTML', alt: 'HTML' },
+      { path:'assets/img/skill-set/scrum.svg', name: 'Scrum', alt: 'Scrum' },
+      { path:'assets/img/skill-set/Css.svg', name: 'CSS', alt: 'CSS' },
+      { path:'assets/img/skill-set/Growth.svg', name: 'Growth mindset', alt: 'Growth mindset' }
+    ]
+  ;
 }

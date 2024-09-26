7dcd2228d6ab5b1633a6c74f53a6febcc08d1fae
Sebastian-Rothe <sebi1995@gmx.at>
2024-09-26 15:00:42 +0200

    working on skill set

--- src/app/main-content/sc3-skill-set/sc3-skill-set.component.ts
+++ src/app/main-content/sc3-skill-set/sc3-skill-set.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 
+interface SkillIcon {
+  icons: {
+    path: string;
+    name: string;
+    alt?: string;
+  }[];
+}
+
 @Component({
   selector: 'app-sc3-skill-set',
   standalone: true,
   imports: [],
   templateUrl: './sc3-skill-set.component.html',
-  styleUrl: './sc3-skill-set.component.scss'
+  styleUrl: './sc3-skill-set.component.scss',
 })
 export class Sc3SkillSetComponent {
-
+  skillSetIcons: SkillIcon = {
+    icons: [
+      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
+      { path:'assets/img/skill-set/Firebase.svg', name: 'Firebase', alt: 'Firebase' },
+      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
+      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
+      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
+      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
+      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
+      { path:'assets/img/skill-set/Api.svg', name: 'Rest-Api', alt: 'Rest-Api' },
+    
+    ],
+  };
 }

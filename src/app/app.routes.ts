38f5c117f6f08190825d156b3eebc29edeebd8e0
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-03 14:41:25 +0200

    deleted navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { Sc2AboutMeComponent } from './main-content/sc2-about-me/sc2-about-me.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
+    { path: 'about-me', component: Sc2AboutMeComponent},
     { path: 'imprint', component: ImprintComponent },
 ];
--- src/app/main-content/sc1-hero/sc1-hero.component.ts
+++ src/app/main-content/sc1-hero/sc1-hero.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
 import { BannerComponent } from './banner/banner.component';
 
 @Component({
   selector: 'app-sc1-hero',
   standalone: true,
-  imports: [NavbarComponent, BannerComponent],
+  imports: [ BannerComponent],
   templateUrl: './sc1-hero.component.html',
   styleUrl: './sc1-hero.component.scss'
 })

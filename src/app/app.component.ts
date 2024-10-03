5289fd014f5ee7225adc2d9d97af5585c3bfd86b
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-03 11:12:38 +0200

    background added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent, ImprintComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent, ImprintComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

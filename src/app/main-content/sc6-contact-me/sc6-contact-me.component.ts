e548e6d17e21ce839c4cc93a1a2021c0ff2f58ee
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-07 14:08:30 +0200

    contact form added

--- src/app/main-content/sc6-contact-me/sc6-contact-me.component.ts
+++ src/app/main-content/sc6-contact-me/sc6-contact-me.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sc6-contact-me',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sc6-contact-me.component.html',
-  styleUrl: './sc6-contact-me.component.scss'
+  styleUrl: './sc6-contact-me.component.scss',
 })
 export class Sc6ContactMeComponent {
+  http = inject(HttpClient);
 
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      console.log(this.contactData);
+
+      ngForm.resetForm();
+    }
+  }
 }

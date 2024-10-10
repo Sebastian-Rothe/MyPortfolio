2b6503eaacf2c323e201ada5d5d5e64590072bda
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-10 21:39:21 +0200

    so soso lalal

--- src/app/main-content/sc5-what-my-colleagues-say/sc5-what-my-colleagues-say.component.ts
+++ src/app/main-content/sc5-what-my-colleagues-say/sc5-what-my-colleagues-say.component.ts
@@ -10,10 +10,38 @@ import { OpinionInterface } from '../../interface/opinion.interface';
   styleUrl: './sc5-what-my-colleagues-say.component.scss',
 })
 export class Sc5WhatMyColleaguesSayComponent {
-  opinion: OpinionInterface[] = [
+  opinions: OpinionInterface[] = [
+    {
+      name: 'Batool Zamani',
+      text: `Ich habe mit Sebastian an zwei Projekten zusammengearbeitet. Er bringt immer positive Energie mit. Er ist sehr gut darin, seine Zeit zu organisieren und findet auch in schwierigen Situationen stets effektive Lösungen.`,
+    },
+    {
+      name: 'Batool Zamani',
+      text: `Ich habe mit Sebastian an zwei Projekten zusammengearbeitet. Er bringt immer positive Energie mit. Er ist sehr gut darin, seine Zeit zu organisieren und findet auch in schwierigen Situationen stets effektive Lösungen.`,
+    },
     {
       name: 'Batool Zamani',
       text: `Ich habe mit Sebastian an zwei Projekten zusammengearbeitet. Er bringt immer positive Energie mit. Er ist sehr gut darin, seine Zeit zu organisieren und findet auch in schwierigen Situationen stets effektive Lösungen.`,
     },
   ];
+
+  currentSlide = 0;
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.opinions.length;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.opinions.length) % this.opinions.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+  }
+
+  getTransform() {
+    // Berechnet den Wert für die CSS-Transform-Eigenschaft
+    return `translateX(-${this.currentSlide * (100 / this.opinions.length)}%)`;
+  }
+  
 }

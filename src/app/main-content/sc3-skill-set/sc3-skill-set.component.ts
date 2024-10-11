63436987c26c2278270bfe9b522535dabc0d07b3
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-11 12:38:11 +0200

    sc3 translation

--- src/app/main-content/sc3-skill-set/sc3-skill-set.component.ts
+++ src/app/main-content/sc3-skill-set/sc3-skill-set.component.ts
@@ -1,14 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { ProjectsService } from '../../services/projects.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../../services/translate.service';
 
 @Component({
   selector: 'app-sc3-skill-set',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './sc3-skill-set.component.html',
   styleUrl: './sc3-skill-set.component.scss',
 })
 export class Sc3SkillSetComponent {
 
   service = inject(ProjectsService);
+  translate = inject(TranslationService);
 }

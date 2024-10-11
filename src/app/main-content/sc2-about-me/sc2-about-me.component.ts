5561956322ad70b1496c3b70ce4db005f10d72f8
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-11 11:09:22 +0200

    sc2 translation

--- src/app/main-content/sc2-about-me/sc2-about-me.component.ts
+++ src/app/main-content/sc2-about-me/sc2-about-me.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../../services/translate.service';
 
 @Component({
   selector: 'app-sc2-about-me',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './sc2-about-me.component.html',
   styleUrl: './sc2-about-me.component.scss'
 })
 export class Sc2AboutMeComponent {
-
+  translate = inject(TranslationService);
 }

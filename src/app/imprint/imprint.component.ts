aaa539c898e2d2d50a0c3a7f59cc67183ed2bf30
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-13 19:18:46 +0200

    imprint translation

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../services/translate.service';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [ TranslateModule ],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
-
+  translate = inject(TranslationService);
 }

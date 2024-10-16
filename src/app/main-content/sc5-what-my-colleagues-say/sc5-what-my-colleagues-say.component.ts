4973c675c7fc51e87eb90b527ca0d86e8364d19a
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-16 12:29:05 +0200

    slide is there, oooh yes!

--- src/app/main-content/sc5-what-my-colleagues-say/sc5-what-my-colleagues-say.component.ts
+++ src/app/main-content/sc5-what-my-colleagues-say/sc5-what-my-colleagues-say.component.ts
@@ -4,11 +4,12 @@ import { OpinionInterface } from '../../interface/opinion.interface';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../../services/translate.service';
 import { ProjectsService } from '../../services/projects.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sc5-what-my-colleagues-say',
   standalone: true,
-  imports: [TranslateModule, SingleOpinionComponent],
+  imports: [TranslateModule, SingleOpinionComponent, CommonModule],
   templateUrl: './sc5-what-my-colleagues-say.component.html',
   styleUrl: './sc5-what-my-colleagues-say.component.scss',
 })
@@ -19,17 +20,18 @@ export class Sc5WhatMyColleaguesSayComponent {
 
   currentSlide = 0;
 
+
   nextSlide() {
-    this.currentSlide = (this.currentSlide + 1) % this.opinions.length;
+    this.currentSlide = (this.currentSlide + 1) % this.opinions.length; // Zur nächsten Slide wechseln
   }
+
   prevSlide() {
-    this.currentSlide = (this.currentSlide - 1 + this.opinions.length) % this.opinions.length;
+    this.currentSlide = (this.currentSlide - 1 + this.opinions.length) % this.opinions.length; // Zur vorherigen Slide wechseln
   }
+
   goToSlide(index: number) {
     this.currentSlide = index;
   }
-  getTransform() {
-    return `translateX(-${this.currentSlide * (100 / this.opinions.length)}%)`;
-  }
+  
   
 }
--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -113,5 +113,35 @@ Insgesamt kann ich mit Überzeugung sagen, dass Sebastians technisches Know-how 
         'I worked with Sebastian on the Kochwelt project. Sebastian is an extremely structured and likeable person. You could tell that from the first few minutes. As fast as he was, he was also focused and determined. I really enjoyed working with Sebastian. He is someone you would always want in a group.',
       occupation: 'Team Partner',
     },
+    {
+      name: 'Batool Zamani',
+      text_de: `Ich habe mit Sebastian an zwei Projekten zusammengearbeitet. Er bringt immer positive Energie mit. Er ist sehr gut darin, seine Zeit zu organisieren und findet auch in schwierigen Situationen stets effektive Lösungen.`,
+      text_en:
+        'I have worked with Sebastian on two projects. He always brings positive energy. He is very good at organizing his time and always finds effective solutions even in difficult situations.',
+      occupation: 'Team Partner',
+    },
+    {
+      name: 'Muaz Yildiz',
+      text_de: `Es ist mir eine Freude, eine Referenz für Sebastian zu verfassen. Während unserer gemeinsamen Zeit im Team habe ich die Gelegenheit gehabt, sein herausragendes Talent für das Coden zu beobachten. Sebastians Fähigkeit, effizienten und gut strukturierten Code zu schreiben, ist beeindruckend und trägt maßgeblich zum Erfolg unserer Projekte bei.
+
+Darüber hinaus hat Sebastian ein ausgezeichnetes Projektmanagement gezeigt und zeichnet sich durch seine Fähigkeit aus, komplexe Aufgaben in überschaubare Schritte zu gliedern. Sein Engagement für klare Projektstrukturen und effektive Arbeitsabläufe hat maßgeblich dazu beigetragen, dass unsere Projekte termingerecht abgeschlossen wurden.
+
+Sebastian ist auch dafür bekannt, sich eigenständig in neue Technologien einzuarbeiten. 
+
+Darüber hinaus muss ich betonen, dass Sebastian nicht nur ein begabter Programmierer ist, sondern auch als Scrum Master außerordentlich erfolgreich agiert. Seine Fähigkeit zur Koordination von Teams und zur Einhaltung von agilen Methoden hat wesentlich dazu beigetragen, dass unsere Projekte reibungslos verlaufen sind.
+
+Insgesamt kann ich mit Überzeugung sagen, dass Sebastians technisches Know-how kombiniert mit seinem Organisationstalent ihn zu einem unschätzbaren Asset in jedem Entwicklungsteam macht. Es war eine Bereicherung, mit ihm zusammenzuarbeiten und ich bin sicher davon überzeugt, dass er auch künftig Großartiges leisten wird.
+`,
+      text_en:
+        'It is a pleasure to write a reference for Sebastian. During our time together in the team, I have had the opportunity to observe his outstanding talent for coding. Sebastians ability to write efficient and well-structured code is impressive and contributes significantly to the success of our projects. In addition, Sebastian has demonstrated excellent project management and excels at breaking down complex tasks into manageable steps. His commitment to clear project structures and effective workflows has been instrumental in ensuring that our projects have been completed on time. Sebastian is also known for his ability to independently familiarize himself with new technologies. In addition, I must emphasize that Sebastian is not only a talented programmer, but is also exceptionally successful as a Scrum Master. His ability to coordinate teams and adhere to agile methods has played a major role in ensuring that our projects have run smoothly. Overall, I can confidently say that Sebastians technical know-how combined with his organizational skills make him an invaluable asset to any development team. It has been a pleasure to work with him and I am sure he will continue to do great things in the future.',
+      occupation: 'Team Partner',
+    },
+    {
+      name: 'Emre Isik',
+      text_de: `Mit Sebastian habe ich am Kochwelt-Projekt zusammengearbeitet. Sebastian ist ein extrem strukturierter und sympathischer Mensch. Das konnte man ihm schon in den ersten Minuten anmerken. So schnell er auch war, so konzentriert und zielstrebig war er ebenfalls. Ich habe sehr gerne mit Sebastian gearbeitet. Er ist jemand, den man sich immer in einer Gruppenarbeit wünschen würde.`,
+      text_en:
+        'I worked with Sebastian on the Kochwelt project. Sebastian is an extremely structured and likeable person. You could tell that from the first few minutes. As fast as he was, he was also focused and determined. I really enjoyed working with Sebastian. He is someone you would always want in a group.',
+      occupation: 'Team Partner',
+    },
   ];
 }

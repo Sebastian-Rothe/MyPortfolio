be236ff5b2ef5ec3566f8c984f23f932b09b5399
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-25 10:05:08 +0200

    working on contact

--- src/app/main-content/sc6-contact-me/sc6-contact-me.component.ts
+++ src/app/main-content/sc6-contact-me/sc6-contact-me.component.ts
@@ -30,8 +30,8 @@ export class Sc6ContactMeComponent {
     body: (payload: any) => JSON.stringify(payload),
     options: {
       headers: {
-        'Content-Type': 'text/plain',
-        responseType: 'text',
+        'Content-Type': 'application/json',
+        responseType: 'text' as 'json',
       },
     },
   };

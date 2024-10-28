92f409c378db53b339bf2982f01533b32c8dc72c
Sebastian-Rothe <sebi1995@gmx.at>
2024-10-28 14:58:41 +0100

    now its all links

--- src/app/main-content/sc3-skill-set/sc3-skill-set.component.ts
+++ src/app/main-content/sc3-skill-set/sc3-skill-set.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ProjectsService } from '../../services/projects.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../../services/translate.service';
+import { ScrollService } from '../../services/scroll.service';
 
 
 @Component({
@@ -16,4 +17,9 @@ export class Sc3SkillSetComponent {
   service = inject(ProjectsService);
   translate = inject(TranslationService);
 
+  constructor(private scrollService: ScrollService) {}
+
+  navigateWithOffset(targetId: string, offset: number = 100) {
+    this.scrollService.navigateWithOffset(targetId, offset);
+  }
 }

d30f8e729d4704c7610e62f6837fb73192f4c2ce
Sebastian-Rothe <sebi1995@gmx.at>
2025-10-16 14:03:05 +0200

    start privacy policy from top

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../services/translate.service';
 
@@ -9,6 +9,11 @@ import { TranslationService } from '../services/translate.service';
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
-export class PrivacyPolicyComponent {
+export class PrivacyPolicyComponent implements OnInit {
   translate = inject(TranslationService);
+
+  ngOnInit() {
+    // Scroll to top when component loads
+    window.scrollTo(0, 0);
+  }
 }
